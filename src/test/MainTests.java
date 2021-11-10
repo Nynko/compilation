fac260ac378dc099927fe5dc7b407e9ff65ba9e8
Tom Gouville <tom.gouville@protonmail.com>
2021-11-10 15:44:12 +0100

    [ADD] Testing

---
+++ src/test/MainTests.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.io.Console;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class MainTests {
+    @Test
+    public void sampleTest() {
+        assertEquals(true, true);
+    }
+}
