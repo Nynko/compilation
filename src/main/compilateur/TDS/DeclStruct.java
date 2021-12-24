842b7c03cee8a0d01e3bbb1170bfdc37e7ba5487
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2021-12-25 00:52:26 +0100

    [UPDATE] Update last commit - Changement DeclStruct avec une liste de symboles correspondant au contenu de la struct

--- src/main/compilateur/TDS/DeclStruct.java
+++ src/main/compilateur/TDS/DeclStruct.java
@@ -1,16 +1,31 @@
 package compilateur.TDS;
 
+import java.util.ArrayList;
 
 public class DeclStruct extends Symbole{
 
     private String name;
+    private ArrayList<Symbole> declVars;
 
     public DeclStruct(String name){
         this.name = name;
+        this.declVars = new ArrayList<Symbole>();
+    }
+
+    public DeclStruct(String name, ArrayList<Symbole> declVars){
+        this.name = name;
+        this.declVars = declVars;
     }
 
     public String getName(){
         return this.name;
     }
     
+    public void addDeclVars(Symbole symbole){
+        this.declVars.add(symbole);
+    }
+
+    public ArrayList<Symbole> getListDeclVars(){
+        return this.declVars;
+    }
 }
