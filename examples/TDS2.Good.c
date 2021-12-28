1f3f99a469e7f9f4b73d054df6ef3e601f122041
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2021-12-28 20:37:32 +0100

    [ADD] Test TDS

---
+++ examples/TDS2.Good.c
@@ -0,0 +1,30 @@
+
+struct vide{
+
+};
+
+struct a{
+    struct vide * b;
+    int a;
+};
+
+int une_fonction(){
+    int a;
+    int b;
+
+    while(1){
+        int c;
+        if(condition){
+            int d;
+        }
+        else{
+            int e;
+        }
+    }
+
+    {
+        if(test){
+            ;
+        }
+    }
+}
