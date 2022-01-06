9f257818e24e6d2e5c6e6062768e29252e2922e4
thibaut hansmann <thibaut.hansmann@telecomnancy.eu>
2022-01-06 15:45:04 +0100

    Fix correction des exception pour undefined symbol

--- src/main/compilateur/TDS/UndefinedSymboleException.java
+++ src/main/compilateur/TDS/UndefinedSymboleException.java
@@ -2,7 +2,7 @@ package compilateur.TDS;
 
 public class UndefinedSymboleException extends SemanticErrorException{
     
-    public UndefinedSymboleException(String symLeftName, String symRightName, int line) {
-        super(line, "Undefined symbol" + symLeftName + "in" + symRightName + "on line" + line + ".");
+    public UndefinedSymboleException(String symName, int line) {
+        super(line, "Undefined symbol" + symName + "on line" + line + ".");
     }
 }
