284d8f2967dabd5354123b7cb1ec7609c5560ef5
Tom Gouville <tom.gouville@protonmail.com>
2022-01-07 08:48:07 +0100

    [ADD] Ajout d'un booléen d'initalisation des variables

--- src/main/compilateur/TDS/SymboleVar.java
+++ src/main/compilateur/TDS/SymboleVar.java
@@ -3,9 +3,18 @@ package compilateur.TDS;
 public abstract class SymboleVar extends Symbole {
     private int paramIndex = -1;
     private int deplacement;
+    private boolean initialise = false;
 
     public SymboleVar(String name) {
         super(name);
+    }
+
+    public boolean isInitalized() {
+        return this.initialise;
+    }
+
+    public void setInitalized() {
+        this.initialise = true;
     }
     
     public void setParam(int index) {
