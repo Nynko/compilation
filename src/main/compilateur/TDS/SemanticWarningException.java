b572613e84b5fd4030998c5791109d19e6e2fec1
Gilles JeanCharles <jean-charles.gilles@telecomnancy.eu>
2022-01-25 20:33:52 +0100

    [ADD] warning comparaison entre 2 types + affectation a un type different

---
+++ src/main/compilateur/TDS/SemanticWarningException.java
@@ -0,0 +1,15 @@
+package compilateur.TDS;
+
+import compilateur.utils.CompilerErrorException;
+
+/**
+ * WarningEception
+ */
+public abstract class SemanticWarningException extends CompilerErrorException {
+
+    public SemanticWarningException(int line, String message) {
+        super(line, "WARNING: " + message);
+    }
+
+    
+}
---
+++ src/main/compilateur/TDS/TypeWarningException.java
@@ -0,0 +1,9 @@
+package compilateur.TDS;
+
+public class TypeWarningException extends SemanticWarningException {
+
+    public TypeWarningException(int line, String typeLeft, String typeRight) {
+        super(line, "incompatible pointer to integer conversion assigning to '"+ typeLeft +"' from '"+ typeRight +"'" );
+    }
+
+}
