5daf904f11ef2bb515a2d3e46f57709daa30d78e
Gilles JeanCharles <jean-charles.gilles@telecomnancy.eu>
2022-01-25 17:01:48 +0100

    [IMP] factorisation des test si les idf sont initialisés

--- src/main/compilateur/ast/SuperieurEgal.java
+++ src/main/compilateur/ast/SuperieurEgal.java
@@ -1,6 +1,6 @@
 package compilateur.ast;
 
-public class SuperieurEgal extends Operateur {
+public class SuperieurEgal extends Comparaison {
 
     public <T> T accept(AstVisitor<T> visitor) {
         return visitor.visit(this);
