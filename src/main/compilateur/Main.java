dccd49ea64db25d65c194cc2ead628c93dbf2587
Gilles JeanCharles <jean-charles.gilles@telecomnancy.eu>
2022-01-25 15:38:37 +0100

    [FIX] generation de l'ast si des erreurs syntaxiques sont presentes

--- src/main/compilateur/Main.java
+++ src/main/compilateur/Main.java
@@ -1,7 +1,12 @@
 package compilateur;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -20,9 +25,9 @@ import compilateur.utils.ErrorAggregator;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        if (args.length < 1){
+        if (args.length < 1) {
             System.out.println("Error : Expected 1 argument filepath, found 0");
             return;
         }
@@ -31,7 +36,7 @@ public class Main {
 
         try {
 
-            //chargement du fichier et construction du parser
+            // chargement du fichier et construction du parser
             CharStream input = CharStreams.fromFileName(testFile);
             circLexer lexer = new circLexer(input);
 
@@ -52,7 +57,7 @@ public class Main {
             // JFrame frame = new JFrame("Antlr AST");
             // JPanel panel = new JPanel();
             // TreeViewer viewer = new TreeViewer(Arrays.asList(
-            //         parser.getRuleNames()),program);
+            //         parser.getRuleNames()), program);
             // viewer.setScale(1.5); // Scale a little
             // panel.add(viewer);
             // frame.add(panel);
@@ -60,45 +65,44 @@ public class Main {
             // frame.pack();
             // frame.setVisible(true);
 
-            // Visiteur de création de l'AST + création de l'AST
-            AstCreator creator = new AstCreator();
-            Ast ast = program.accept(creator);
-
             // Récupération des erreurs syntaxiques et lexicales
             ErrorAggregator agg = errList.getAggregator();
 
+            if (agg.noError()) {
+                // Visiteur de création de l'AST + création de l'AST
+                AstCreator creator = new AstCreator();
+                Ast ast = program.accept(creator);
 
+                if (agg.noError()) {
+                    // Visiteur de représentation graphique + appel
+                    GraphVizVisitor graphViz = new GraphVizVisitor();
+                    ast.accept(graphViz);
+                    graphViz.dumpGraph("./out/tree.dot");
 
-            if(agg.noError()) {
-                // Visiteur de représentation graphique + appel
-                GraphVizVisitor graphViz = new GraphVizVisitor();
-                ast.accept(graphViz);
-                graphViz.dumpGraph("./out/tree.dot");
+                    // TDS
+                    GraphVizTdsVisitor graphVizTds = new GraphVizTdsVisitor();
+                    Tds tds = new Tds("prog");
+                    TdsCreator tdsCreator = new TdsCreator();
+                    tdsCreator.setErrorAggregator(agg);
+                    ast.accept(tdsCreator, tds);
 
-                // TDS
-                GraphVizTdsVisitor graphVizTds = new GraphVizTdsVisitor();
-                Tds tds = new Tds("prog");
-                TdsCreator tdsCreator = new TdsCreator();
-                tdsCreator.setErrorAggregator(agg);
-                ast.accept(tdsCreator, tds);
+                    graphVizTds.createGraph(tds);
+                    graphVizTds.dumpGraph("./out/tds.dot");
 
-                graphVizTds.createGraph(tds);
-                graphVizTds.dumpGraph("./out/tds.dot");
-
+                }
+            } else {
+                System.out.println("erreur syntaxique");
             }
             System.out.println("==== Erreurs ====");
             agg.printErrors();
             System.out.println("==== Erreurs ====\n\n");
 
-        } 
-        catch (IOException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (RecognitionException e) {
             e.printStackTrace();
         }
-        catch (RecognitionException e) {
-            e.printStackTrace();
-        }
-        
 
     }
-    
+
 }
