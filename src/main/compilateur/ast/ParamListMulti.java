b554b5d0a7d44ccbff86205ff4096f1178c74eb4
Tom Gouville <tom.gouville@protonmail.com>
2022-02-22 14:27:26 +0100

    [ADD] Registering tds of each nod on the ast

--- src/main/compilateur/ast/ParamListMulti.java
+++ src/main/compilateur/ast/ParamListMulti.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import compilateur.ARMGenerator.ARMVisitor;
 import compilateur.tds.*;
 
-public class ParamListMulti implements Ast {
+public class ParamListMulti extends AstNode implements Ast {
 
     public <T> T accept(AstVisitor<T> visitor) {
         return visitor.visit(this);
