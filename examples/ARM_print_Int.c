8e4923951f985646faba0f9037642caa1290a1b7
tom <tom@ubuntu>
2022-03-31 02:04:52 +0000

    test

--- examples/ARM_print_Int.c
+++ examples/ARM_print_Int.c
@@ -1,6 +1,26 @@
 
+int func(int c){
+    if(c==0){
+        return 0;
+    }
+    else{
+        return func(c-1)+1;
+    }
+
+}
 int main(){
     int a;
-    a = 12 + 3;
+    a = func(100);
+    print(a);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
