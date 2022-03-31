d00c84c7a656225ebf743ca163e12dbb33f74940
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-03-31 03:04:27 +0200

    [TEST] linux

--- src/main/compilateur/ARMGenerator/TrueARM64Generator.java
+++ src/main/compilateur/ARMGenerator/TrueARM64Generator.java
@@ -687,9 +687,9 @@ public class TrueARM64Generator implements AstVisitor<String> {
         }
         else{ // linux
             str.appendLine("""
-                mov	    X8, X0  // Argument dans X0, on passe à printf par X8
+                mov	    X9, X0  // Argument dans X0, on passe à printf par X8
                 ldr 	x0, =l_.str
-                str	    x8, [sp,#-16]!
+                str	    x9, [sp,#-16]!
                 bl	printf
                 add SP, SP, #16 // Clean up stack
                     """);
