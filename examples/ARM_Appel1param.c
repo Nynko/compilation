2431484fe4a81603da00b9fd00f8faf4837c3f9b
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-04-09 17:25:10 +0200

    [ADD] tests

---
+++ examples/ARM_Appel1param.c
@@ -0,0 +1,19 @@
+
+int func(int c){
+
+    print(c);
+}
+int main(){
+    func(45);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ examples/ARM_Appel5params.c
@@ -0,0 +1,24 @@
+
+int func(int c, int d, int e, int f, int g){
+
+    print(c);
+    print(d);
+    print(e); 
+    print(f);
+    print(g);
+    return 0;
+}
+int main(){
+    func(45,43 ,56, 67, 78);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
