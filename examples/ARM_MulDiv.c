0d2323626f877f1f5d20213273538c3c0616b979
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-04-13 04:21:52 +0200

    [TESTs]

---
+++ examples/ARM_MulDiv.c
@@ -0,0 +1,10 @@
+
+
+int main(){
+
+    int a;
+    a = 3*4/2*4/4/2;
+    // 3
+    print(a);
+    return 0;
+}
---
+++ examples/ARM_priorite.c
@@ -0,0 +1,19 @@
+
+int main(){
+    int a;
+    a =  3 + 4 * 6;
+    print(3 + 4 * 6);
+    print(4*6);
+    print(a);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
