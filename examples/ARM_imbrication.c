29899fd3cc37637224c22ebd128909b4153f5587
Nicolas Beaudouin <nicolas.beaudouin@telecomnancy.eu>
2022-04-14 03:38:22 +0200

    [TEST]

--- examples/ARM_imbrication.c
+++ examples/ARM_imbrication.c
@@ -5,10 +5,12 @@ int main(){
     int b;
     int c;
     int d;
+    int compt;
     a = 3;
     c = 2;
     {
         int a;
+        compt = 0;
         a = 4;
         print(a); // 4
         {
@@ -20,6 +22,13 @@ int main(){
             while (d < 5)
             {
                 d = d + 1;
+
+                if(d == 1){
+                    compt = compt + 1;
+                }
+                else{
+                    compt = compt - 1;
+                }
             }
         }
         
@@ -28,5 +37,6 @@ int main(){
     print(b); // 6
     print(d); // 5
     print(c); // 0
+    print(compt); // -3
     return 0;
 }
